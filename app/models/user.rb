da52a9a132af8317bff2d66c0ffc4d61905d9fad
Konishi <koni.nwtl@gmail.com>
2019-08-25 13:50:31 +0000

    rails 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { minimum: 1, maximun: 15 }
     validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/ }
     validates :password, presence: true, length: { minimum: 8, maximum: 32 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
-    validates :password_confirmation, presence: true, length: { minimum: 8, maximum: 32 }
-    
+
     has_secure_password
 end
